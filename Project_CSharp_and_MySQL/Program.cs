85b826de75fce4f8002486854bf91f5aad13fd12
lloydbutler <62755237+lloydbutler@users.noreply.github.com>
2021-07-28 15:40:34 +0100

    Add files via upload

---
+++ Project_CSharp_and_MySQL/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Project_CSharp_and_MySQL
+{
+    class Program
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            Shopping shop = new Shopping();
+
+            Console.WriteLine("Please select from the following options:");
+            Console.WriteLine("1. Data Entry");
+            Console.WriteLine("2. Reports");
+            Console.WriteLine("3. Close");
+            string opt = Console.ReadLine();
+
+            if (Convert.ToInt32(opt) == 1)
+            {
+                Console.WriteLine("Please enter product name");
+                string proName = Console.ReadLine();
+                Console.WriteLine("Please enter quantity sold");
+                string qtySold = Console.ReadLine();
+                Console.WriteLine("Please enter the price");
+                string price = Console.ReadLine();
+                
+                shop.addSale(proName, Convert.ToInt32(qtySold), float.Parse(price));
+            }
+
+            if (Convert.ToInt32(opt) == 2)
+            {
+                Console.WriteLine("1. list products sold in a specific year");
+                Console.WriteLine("2. list of products sold in a specific month within a specific year");
+                Console.WriteLine("3. total sales in a specific year");
+                Console.WriteLine("4. total sales in a specific month");
+                
+                string select = Console.ReadLine();
+
+                if (Convert.ToInt32(select) == 1)
+                {
+                    Console.WriteLine("please type year to view");
+                    int ytv = Convert.ToInt32(Console.ReadLine());
+                    shop.SaleReport(0,ytv, Convert.ToInt32(select));
+                }
+                if (Convert.ToInt32(select) == 2)
+                {
+                    Console.WriteLine("please type year to view");
+                    int ytv = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("please type month to view");
+                    int mtv = Convert.ToInt32(Console.ReadLine());
+                    shop.SaleReport(mtv,ytv, Convert.ToInt32(select));
+                }
+                if (Convert.ToInt32(select) == 3)
+                {
+                    Console.WriteLine("please type year to view");
+                    int ytv = Convert.ToInt32(Console.ReadLine());
+                    shop.SaleReport(0,ytv, Convert.ToInt32(select));
+                }
+                if (Convert.ToInt32(select) == 4)
+                {
+                    Console.WriteLine("please type year to view");
+                    int ytv = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("please type month to view");
+                    int mtv = Convert.ToInt32(Console.ReadLine());
+                    shop.SaleReport(mtv, ytv, Convert.ToInt32(select));
+                }
+                
+
+            }
+            if (Convert.ToInt32(opt) == 3)
+            {
+                Environment.Exit(0);
+            }
+
+
+        }
+    }
+}
---
+++ Project_CSharp_and_MySQL/Shopping.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Project_CSharp_and_MySQL
+{
+    
+    class Shopping
+    {
+        MySqlConnection con;
+        MySqlCommand cmd;
+        
+        
+
+        public Shopping()
+        {
+            string connectionstring = "server=localhost;user=root;password=root;database=nbs4";
+            con = new MySqlConnection(connectionstring);
+            con.Open();
+            cmd = new MySqlCommand();
+            cmd.Connection = con;
+            
+        }
+        public void addSale(string ProductName, int Quantity, float price)
+        {
+            cmd.CommandText = $"insert into shop (productname, quantity, price, saledate) values('{ProductName}', {Quantity}, {price}, now())";
+            cmd.ExecuteNonQuery();
+            Console.ReadLine();
+            
+        }
+        public void SaleReport(int mchoice, int ychoice, int select)
+        {
+
+            string row = "";
+            if (select == 1)
+            {
+                
+                cmd.CommandText = $"select * from shop where year(saledate)={ychoice}";
+                cmd.ExecuteNonQuery();
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                
+                while (myReader.Read())
+                {
+                    for (int i = 0; i < myReader.FieldCount; i++)
+                    {
+                        row += myReader.GetName(i) + "\t";
+                    }
+                    Console.WriteLine(row);
+                    row = "";
+
+                    for (int i = 0; i < myReader.FieldCount; i++)
+                    {
+                        row += myReader.GetString(i) +"\t";
+                    }
+                    Console.WriteLine(row);
+                    row = "";       
+                }
+
+                myReader.Close();
+                Console.ReadLine();
+            }
+            if (select == 2)
+            {
+                cmd.CommandText = $"select * from shop where year(saledate)={ychoice} and month(saledate)={mchoice}";
+                cmd.ExecuteNonQuery();
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                while (myReader.Read())
+                {
+                    for (int i = 0; i < myReader.FieldCount; i++)
+                    {
+                        row += myReader.GetString(i) + " ";
+                    }
+                    Console.WriteLine(row);
+                    row = "";
+                }
+
+                myReader.Close();
+                Console.ReadLine();
+            }
+            if (select == 3)
+            {
+                cmd.CommandText = $"select productname, sum(quantity * price) as Total from shop where year(saledate)={ychoice} group by productname";
+                cmd.ExecuteNonQuery();
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                while (myReader.Read())
+                {
+
+                    for (int i = 0; i < myReader.FieldCount; i++)
+                    {
+                        row += myReader.GetString(i) + " ";
+                    }
+                    Console.WriteLine(row);
+                    row = " ";
+
+                }
+
+                myReader.Close();
+                Console.ReadLine();
+            }
+            if (select == 4)
+            {
+                cmd.CommandText = $"select productname, sum(quantity * price) as Total from shop where year(saledate)={ychoice} and month(saledate)={mchoice} group by productname";
+                cmd.ExecuteNonQuery();
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                while (myReader.Read())
+                {
+                    for (int i = 0; i < myReader.FieldCount; i++)
+                    {
+                        row += myReader.GetString(i) + " ";
+                    }
+                    Console.WriteLine(row);
+                    row = "";
+                }
+
+                myReader.Close();
+                Console.ReadLine();
+            }
+
+
+        }
+
+
+
+    }
+}
